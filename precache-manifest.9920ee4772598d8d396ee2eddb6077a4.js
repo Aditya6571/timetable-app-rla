9137e4cdcd5544dabf3acba0b5339820f92c6f9f
diksha-giri013 <adityagiri657@yahoo.in>
2020-10-14 17:33:15 +0530

    Updates

---
+++ precache-manifest.9920ee4772598d8d396ee2eddb6077a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f93a1ec95046ee5ee937ea50731bf96",
+    "url": "/timetable-app-rla/index.html"
+  },
+  {
+    "revision": "cb9870d19b9fa7f1f5ed",
+    "url": "/timetable-app-rla/static/css/main.bde40e2b.chunk.css"
+  },
+  {
+    "revision": "c4c84d34344f8c2a6811",
+    "url": "/timetable-app-rla/static/js/2.c0dfb198.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/timetable-app-rla/static/js/2.c0dfb198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb9870d19b9fa7f1f5ed",
+    "url": "/timetable-app-rla/static/js/main.b10fd1bc.chunk.js"
+  },
+  {
+    "revision": "d13ad3e983afdff4b01c",
+    "url": "/timetable-app-rla/static/js/runtime-main.3c43aef9.js"
+  }
+]);
